6a2ab25f9fe97959ae0a5c77cb5d00be9ab880f5
xyeras <saika_fujimoto@mail.fresnostate.edu>
2021-01-12 19:12:28 -0800

    created boilerplate; created two console logs

---
+++ js/script.js
@@ -0,0 +1,10 @@
+console.log(1);
+
+// This is a single line comment
+
+/*
+This is a multi-line comment
+*/
+
+console.log("My name is Elizabeth.");
+console.log("My favorite food is Asian food." );
