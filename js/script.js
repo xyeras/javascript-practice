863af5486693570b06b17c1bfc2f074b8300e46e
xyeras <saika_fujimoto@mail.fresnostate.edu>
2021-01-14 17:56:23 -0800

    testing out variables and equations

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
-console.log(1);
-
 // This is a single line comment
 
 /*
 This is a multi-line comment
 */
+
+let num = 10;
+console.log(num);
+
+let a = 5;
+let b = 6;
+let c = 7;
+let d = a + b + c;
+console.log(d);
 
 console.log("My name is Elizabeth.");
 console.log("My favorite food is Asian food." );
