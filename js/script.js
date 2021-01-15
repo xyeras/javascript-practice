31fe73e71185f90fa622e8c316e90dc0a1214abe
xyeras <saika_fujimoto@mail.fresnostate.edu>
2021-01-14 18:38:23 -0800

    messing with expressions and data types

--- js/script.js
+++ js/script.js
@@ -15,3 +15,20 @@ console.log(d);
 
 console.log("My name is Elizabeth.");
 console.log("My favorite food is Asian food." );
+
+console.log(10);
+
+let phrase = "Supercalifragilisticexpialidocious";
+console.log(phrase.length);
+console.log(typeof phrase.length);
+
+console.log("sushi".length);
+console.log(typeof "sushi".length);
+
+let e = 100;
+console.log(e);
+console.log(typeof e);
+
+let f = 123456789123456789123456789n;
+console.log(f);
+console.log(typeof f);
